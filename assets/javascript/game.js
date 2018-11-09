61332c5769b7ae8a3bf02b43146254dda68df11d
rBrodeFrank <ryanafrank@gmail.com>
2018-11-08 20:00:36 -0600

    Wrote out the pseudo code in js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+//Javascript framework
+//Theme: Fantasy
+
+
+var game = {
+    wordList: [],
+
+    //function to select random word from wordList
+}
+
+//Array with list of words
+//randomly select a word
+//display word
+
+//document.onkeyup function () 
+//need on key up
+    //get key pressed
+    //check if already guessed
+    //if guessed already alert user
+    //else check if it is in word
+        //if in word reveal the matching letters
+        //else inform user
+        
+        //decriment guesses
+        
+        //check if entire word is revealed
+            //you win
+            //restart game
+        //else check if out of guesses
+            //if so you lose
+            //restart game
+
+//list letters guessed
+//list num guesses remaining
